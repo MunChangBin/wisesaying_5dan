edfa445a92a335214b4b921eeb586d139d66fb1d
깃MunChangBin <vv4704@gmail.com>
2023-03-27 18:10:33 +0900

    level1_attack

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        //sc라는 변수를  this.sc라는 변수에 지정한다.or 담는다.
+        this.sc = sc;
+    }
+
+    public void attack() {
+        //참일경우 반복적으로 실행한다.
+        while (true){
+            //명령어를 담아야한다.
+            System.out.println("공격해바");;
+            String commande = sc.nextLine(); //sc라는 변수를 커맨드라는 명령어로 담는다.
+            //그다음은 조건을 적용한다. 어떻게? if로
+            if (commande.equals("공격")){
+                System.out.println("공격합니다");
+                break;
+            }else {
+                System.out.println("해당기능을 수행할수없습니다.");
+                System.out.println("올바른 명령을 입력하시오");
+            }
+        }
+        System.out.println("종료");
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
+import org.example.App;
+
 import java.util.Scanner;
-
-
+//명령이 공격이면 공격합니다 출력하고  프로그램 종료 그리고 잘못입력하면 (공격이 아니면 해당기능을 수행할수없습니다.)
+//올바른 명령을 입력하시오.
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String sc = in.nextLine();
+        Scanner sc = new Scanner(System.in);
+        //app 클래스 안에. 메서드 어택을 지정한다.
+
+        new App(sc).attack();
+
 
     }
 }
